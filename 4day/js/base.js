5ef52db30e8d83d07b1f67be9b4332512ab3c019
CATK\S12173 <sato_sho_xa@abema.tv>
2020-12-11 12:51:17 +0900

    no message

--- 4day/js/base.js
+++ 4day/js/base.js
@@ -1,25 +1,25 @@
 
-// $(function() {
-// 	countDown();
-// });
+$(function() {
+	countDown();
+});
 
-// function countDown() {
-// 	var now = new Date();
-// 	var time = new Date();
-// 	var end = new Date("2020/12/31 00:00");
-// 	var left = end - time;
-// 	var a_day = 24 * 60 * 60 * 1000;
-// 	var d = Math.floor(left / a_day)
-// 	var h = Math.floor((left % a_day) / (60 * 60 * 1000))
-// 	var m = Math.floor((left % a_day) / (60 * 1000)) % 60
-// 	var s = Math.floor((left % a_day) / 1000) % 60 % 60
+function countDown() {
+	var now = new Date();
+	var time = new Date();
+	var end = new Date("2020/12/31 00:00");
+	var left = end - time;
+	var a_day = 24 * 60 * 60 * 1000;
+	var d = Math.floor(left / a_day)
+	var h = Math.floor((left % a_day) / (60 * 60 * 1000))
+	var m = Math.floor((left % a_day) / (60 * 1000)) % 60
+	var s = Math.floor((left % a_day) / 1000) % 60 % 60
 
-// 	$("#day").text(d);
-// 	$("#hour").text(h);
-// 	$("#min").text(m);
-// 	$("#sec").text(s);
-// 	setTimeout('countDown()', 1000);
-// }
+	$("#day").text(d);
+	$("#hour").text(h);
+	$("#min").text(m);
+	$("#sec").text(s);
+	setTimeout('countDown()', 1000);
+}
 
 // 非表示時間
 // $(document).ready(function() {
